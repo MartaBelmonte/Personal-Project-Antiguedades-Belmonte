7ee821df3bb1e7686b9d4de5040437e98574055e
MartaBelmonte <martabelmonts5@gmail.com>
2024-02-17 21:29:01 +0100

    styles v2

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,21 +6,21 @@ const Contact = () => {
         <section id="contacta" className="main-section">
           <h2 style={{ textAlign: 'center', margin:'20px' }}>Contacta</h2>
           <form className="form">
-            <label>
-              Tu nombre completo:
+            <div className='input-group'>
+              <label>Tu nombre completo:</label>
               <input type="text" name="nombre" />
-            </label>
-            <br />
-            <label>
-              Correo electrónico:
+            </div>
+            
+            <div className='input-group'>
+              <label>Correo electrónico:</label>
               <input type="email" name="email" />
-            </label>
-            <br />
-            <label>
-              ¿Qué nos quieres preguntar?
-              <textarea name="mensaje"></textarea>
-            </label>
-            <br />
+            </div>
+          
+          <div className='input-group'>
+              <label>¿Qué nos quieres preguntar?</label>
+              <textarea name="mensaje" rows='5'></textarea>
+           </div> 
+           
             <button class="submit-button" type="submit">Enviar</button>
           </form>
         </section>
